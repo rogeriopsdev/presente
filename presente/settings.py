1f1e410d392d4d7b911a3b643e03764f6061b4b2
dev <rogerio.pereira@ifto.edu.br>
2024-05-27 20:32:43 -0300

    versão 2.1

--- presente/settings.py
+++ presente/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'presenteApp',
-
+    "crispy_forms",
     "crispy_bootstrap5",
+
+
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
